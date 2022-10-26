c78c55b9a1df5cf28edfca715ee024264608b3d5
nebeltag <nebeltag@mail.ru>
2022-10-26 20:12:18 +0300

    homework_8(complete)

---
+++ homework_8(WP)/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mode: 'development',
+  entry: ['./public/js/main.js', './public/js/CartComponent.js', './public/js/MyError.js', './public/js/productFilter.js', './public/js/ProductComponent.js'],
+  output: {
+    filename: "./build.js"
+  },
+}
